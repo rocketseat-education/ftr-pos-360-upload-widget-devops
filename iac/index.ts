dff6d4ff1ef41a942490bf21d9c73317ed42c2cb
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-01-27 19:02:23 -0300

    Destruindo recursos

--- iac/index.ts
+++ iac/index.ts
@@ -7,14 +7,6 @@ const firstBucket = new aws.s3.BucketV2("primeiro-bucket", {
     },
 });
 
-const secondBucket = new aws.s3.BucketV2("segundo-bucket", {
-    bucket: 'segundo-bucket-pos-full-stack-360',
-    tags: {
-        IAC: "true",
-    },
-});
-
-
 const ecr = new aws.ecr.Repository("primeiro-ecr", {
     name: 'primeiro-ecr',
     imageTagMutability: 'IMMUTABLE',
@@ -26,10 +18,6 @@ const ecr = new aws.ecr.Repository("primeiro-ecr", {
 export const firstBucketName = firstBucket.id;
 export const firstBucketRegion = firstBucket.region;
 export const firstBucketArn = firstBucket.arn;
-
-export const secondBucketName = secondBucket.id;
-export const secondBucketRegion = secondBucket.region;
-export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
