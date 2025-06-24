cd85fbcc358c60caca2fbebdd5205b9fc0e3342b
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-06-24 14:04:41 -0300

    Configurando o SSM

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,11 @@ import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
 import { uploadImageRoute } from './routes/upload-image'
 import { fastifyMultipart } from '@fastify/multipart'
-import secret from './infra/secret'
+//import secret from './infra/secret'
 import { log } from './infra/logger'
+import { SSMClient } from '@aws-sdk/client-ssm'
+
+const client = new SSMClient({ region: 'us-east-2' });
 
 const server = fastify()
 
@@ -15,7 +18,9 @@ server.register(fastifyMultipart)
 server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(async () => {
-  const values = await secret.read('/secret/data/widget-server-stg');
-  console.log(values.data.data)
+  //const values = await secret.read('/secret/data/widget-server-stg');
+  //console.log(values.data.data)
+  const values = await client.send(new GetParameterCommand( { Name: 'CLOUDFLARE_PUBLIC_URL' } ))
+  console.log(values.Parameter?.Value)
   log.info('HTTP server running!!!')
 })
