c6a856e1d76e09e61a868a5aba613c986908c4da
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-06-24 13:12:00 -0300

    Configurando a aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,16 @@ import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
 import { uploadImageRoute } from './routes/upload-image'
 import { fastifyMultipart } from '@fastify/multipart'
+import Vault from 'node-vault'
 import { log } from './infra/logger'
+
+const options: Vault.VaultOptions = {
+  apiVersion: 'v1',
+  endpoint: 'http://127.0.0.1:8200',
+  token: 'root'
+}
+
+const vault = Vault(options)
 
 const server = fastify()
 
@@ -13,6 +22,8 @@ server.register(fastifyCors, {
 server.register(fastifyMultipart)
 server.register(uploadImageRoute)
 
-server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+server.listen({ port: 3333, host: '0.0.0.0' }).then(async () => {
+  const values = await vault.read('/secret/data/widget-server-stg');
+  console.log(values.data.data)
   log.info('HTTP server running!!!')
 })
